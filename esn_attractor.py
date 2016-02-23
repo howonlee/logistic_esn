faf03f94fd8a677955044b50fa50ca446605be6b
Howon Lee <howon@howonlee.com>
2016-02-23 11:57:10 -0800

    see a pretty nice thought

--- esn_attractor.py
+++ esn_attractor.py
@@ -1,10 +1,19 @@
 import numpy as np
 import numpy.random as npr
+import matplotlib.pyplot as plt
 
 # plain esn
 
-curr_hidden = np.random(100) - 0.5
+curr_hidden = npr.random(100) - 0.5
 weights = npr.random((100,100)) - 0.5
 
 if __name__ == "__main__":
-    pass
+    time = 1000
+    activation_progression = []
+    activation_progression_2 = []
+    for x in xrange(time):
+        curr_hidden = np.tanh(np.dot(weights, curr_hidden))
+        activation_progression.append(curr_hidden[0])
+        activation_progression_2.append(curr_hidden[1])
+    plt.scatter(activation_progression, activation_progression_2)
+    plt.show()
