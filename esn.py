5f5c92c071120d524b95690a53eebe365c4b6abb
Howon Lee <howon@howonlee.com>
2016-02-24 08:51:33 -0800

    organize some thoughts

--- esn.py
+++ esn.py
@@ -2,6 +2,20 @@ import numpy as np
 import numpy.random as npr
 import matplotlib.pyplot as plt
 
+def compute_activations():
+    activations = np.zeros(something)
+    activations = np.tanh(np.dot(weights, curr_hidden))
+    return activations
+
+def run_network():
+    pass
+
+def train_out_weights():
+    pass
+
+def test():
+    pass
+
 if __name__ == "__main__":
     num_hiddens = 100
     num_ins, num_outs = 1, 1
@@ -10,18 +24,4 @@ if __name__ == "__main__":
     curr_hidden = npr.random(num_hiddens) - 0.5
     weights_ir = npr.random((num_ins, num_hiddens)) - 0.5
     weights_rr = npr.random((num_hiddens, num_hiddens)) - 0.5
-    weights_ro = npr.random((num_hiddens, num_outs)) - 0.5 # there get learned
-    # try the activations, basically
-    # for n in xrange(2,100):
-    #     time = 10000
-    #     curr_hidden = npr.random(n) - 0.5
-    #     weights = npr.random((n,n)) - 0.5
-    #     activation_progression = []
-    #     activation_progression_2 = []
-    #     for x in xrange(time):
-    #         curr_hidden = np.tanh(np.dot(weights, curr_hidden))
-    #         activation_progression.append(curr_hidden[0])
-    #         activation_progression_2.append(curr_hidden[1])
-    #     plt.close()
-    #     plt.scatter(activation_progression, activation_progression_2)
-    #     plt.show()
+    weights_ro = npr.random((num_hiddens, num_outs)) - 0.5 # these get learned
