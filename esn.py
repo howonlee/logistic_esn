2aaaddceac0e24e3e47e0d89308d2819bcaf2622
Howon Lee <howon@howonlee.com>
2016-02-24 08:47:50 -0800

    roll one from scratch, it will do you good

--- esn.py
+++ esn.py
@@ -3,10 +3,15 @@ import numpy.random as npr
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    num_hiddens = 100
+    num_ins, num_outs = 1, 1
     data = np.loadtxt("MackeyGlass_t17.txt")[:8000]
     train, test = data[:1000], data[1000:]
-    plt.plot(test)
-    plt.show()
+    curr_hidden = npr.random(num_hiddens) - 0.5
+    weights_ir = npr.random((num_ins, num_hiddens)) - 0.5
+    weights_rr = npr.random((num_hiddens, num_hiddens)) - 0.5
+    weights_ro = npr.random((num_hiddens, num_outs)) - 0.5 # there get learned
+    # try the activations, basically
     # for n in xrange(2,100):
     #     time = 10000
     #     curr_hidden = npr.random(n) - 0.5
