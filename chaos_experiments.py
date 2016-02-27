1794f10a253b4eb082f9994863a70152d96c3d34
Howon Lee <howon@howonlee.com>
2016-02-26 19:39:52 -0800

    experiment with simple and less simple basis stuff

--- chaos_experiments.py
+++ chaos_experiments.py
@@ -9,33 +9,44 @@ def saturate(val):
     val[val > 1] = 1
     return val
 
-if __name__ == "__main__":
-    # state = npr.random(100) - 0.5
-    data = np.loadtxt('MackeyGlass_t17.txt')
-    # for _ in xrange(10):
-    #     npr.seed(int(time.time()))
-    #     fold_in_data = lambda state, data: saturate(state + 0.01*data)
-    #     state = npr.random(100)
-    #     activation = []
-    #     for x in xrange(10000):
-    #         # state = np.tanh(np.dot(state, esn))
-    #         state = 2.5 * np.multiply(fold_in_data(state, data[x]), 1 - fold_in_data(state, data[x]))
-    #         if x > 2000:
-    #             activation.append(state[2])
-    #     plt.close()
-    #     plt.scatter(activation[:-1], activation[1:], alpha=0.2)
-    #     plt.show()
+def test_esn(data):
     for _ in xrange(10):
-        state = npr.random(100)
+        state = npr.random(100) - 0.5
         esn = npr.random(size=(100,100)) - 0.5
+        w_in = npr.random(size=(100, 1)) - 0.5
         spec = np.max(np.abs(npl.eig(esn)[0]))
         esn /= spec
         esn *= 1.3
         activation = []
-        for x in xrange(10000):
-            state = np.tanh(np.dot(state, esn) + data[x])
-            if x > 2000:
+        for x in xrange(2000):
+            state = np.tanh(np.dot(state, esn) + np.dot(w_in, data[x]))
+            if x % 500 == 0:
+                print x
+            if x > 500:
                 activation.append(state[3])
         plt.close()
         plt.scatter(activation[:-1], activation[1:], alpha=0.2)
         plt.show()
+
+def test_logistic(data):
+    for _ in xrange(10):
+        npr.seed(int(time.time()))
+        fold_in_data = lambda state, data: saturate(state + data)
+        state = npr.random(100)
+        w_in = npr.random(size=(100, 1)) - 0.5
+        activation = []
+        for x in xrange(2500):
+            state = 2.5 * np.multiply(fold_in_data(state, np.dot(w_in, data[x])), 1 - fold_in_data(state, np.dot(w_in, data[x])))
+            if x % 500 == 0:
+                print x
+            if x > 500:
+                activation.append(state[2])
+        plt.close()
+        plt.scatter(activation[:-1], activation[1:], alpha=0.2)
+        plt.show()
+
+if __name__ == "__main__":
+    # state = npr.random(100) - 0.5
+    data = np.loadtxt('MackeyGlass_t17.txt')
+    test_logistic(data)
+    # test_esn(data)
